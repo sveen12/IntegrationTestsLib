27882e8a4e1298f597d7a29d092c42680290c62e
sveen12 <sveen12@gmail.com>
2023-03-29 14:52:57 -0500

    initial commit

--- integration-testing.ps1
+++ integration-testing.ps1
@@ -1,2 +1,4 @@
-docker-compose -p microservices-environment up -d
-($env:ServicesUrl__MultiplicationService="http://localhost:8002") | dotnet test
+docker-compose -f ./libs/IntegrationTestsLib/docker-compose.yml stop
+docker-compose -f ./libs/IntegrationTestsLib/docker-compose.yml down --rmi local
+docker-compose -f ./libs/IntegrationTestsLib/docker-compose.yml -p microservices-environment up -d
+dotnet test
